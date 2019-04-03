6439d74dedf2d2af086110de61df1d831714fe75
babohorquez10 <ba.bohorquez10@uniandes.edu.co>
2019-04-03 11:05:17 -0500

    Sonidos.

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+
+        source.Play();
+    }
+}
